86e20c50ebaedb31601b7e72dcd0915c6efe4686
havel32 <132665591+havel32@users.noreply.github.com>
2024-09-20 15:54:49 +0000

    Exercise 4

--- lab1/src/average.sh
+++ lab1/src/average.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+if [ "$#" -eq 0 ]; then
+  echo "Укажите файл с числами в качестве аргумента."
+  exit 1
+fi
+if [ ! -f "$1" ]; then
+  echo "Файл '$1' не найден."
+  exit 1
+fi
+
+
 sum=0
 count=0
-while read arg
-do
-echo "ghgh"
-sum=$(($sum + $arg))
-count=$(($count + 1))
-done
-avg_a=$(bc<<<"scale=3;$sum/$count")
-echo "Count: $count"
-echo "Average: $D"
+
+
+while read -r arg; do
+  if [[ $arg =~ ^-?[0-9]+$ ]]; then
+    sum=$((sum + arg))
+    count=$((count + 1))
+  else
+    echo "Найдены недопустимые данные: $arg"
+  fi
+done < "$1"
+
+if [ "$count" -eq 0 ]; then
+  echo "Нет допустимых чисел для вычисления среднего."
+  exit 1
+fi
+
+avg_a=$(bc <<< "scale=3; $sum / $count")
+
+echo "Количество: $count"
+echo "Среднее арифметическое: $avg_a"
