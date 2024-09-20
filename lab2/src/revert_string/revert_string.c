4d8da441cfb04f7d9f026b6bb6f36561900fff11
havel32 <132665591+havel32@users.noreply.github.com>
2024-09-20 21:14:41 +0000

    first exercise done + sothing goes wrong with the second

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,28 @@
 #include "revert_string.h"
 
+
+int len(char *str)
+{
+    int lengh = 0;
+    while (*(str+lengh) != '\0'){
+        lengh++;
+    }
+    return lengh;
+}
+
+
 void RevertString(char *str)
 {
-	// your code here
+	int str_len = len(str);
+	int left = 0;
+	int right = str_len - 1;
+	while (left < right){
+		char buff = str[left];
+		str[left] = str[right];
+		str[right] = buff;
+		left++;
+		right--;
+	}
+
 }
 
